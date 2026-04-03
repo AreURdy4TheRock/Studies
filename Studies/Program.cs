b3ab030fb40a43915d764d4e02db429c97920de2
AreURdy4TheRock <dorkwingdock@gmail.com>
2026-04-03 23:58:45 +0300

    Добавлены переменные и константы. Так же вывод в консоль.

--- Studies/Program.cs
+++ Studies/Program.cs
@@ -4,8 +4,21 @@
     {
         static void Main(string[] args)
         {
-            string MyName = "Art";
-            Console.WriteLine(MyName);
+            string myName = "Артём";
+            int myAge = 32;
+            const bool myPet = false;
+            double sizeLeg = 39.5;
+
+            Console.WriteLine("Меня зовут {0}", myName);
+            Console.WriteLine("Мне {0} года", myAge);
+
+            if (myPet)
+            {
+                Console.WriteLine("У меня есть питомец");
+            }
+            else Console.WriteLine("У меня нет питомца");
+
+            Console.WriteLine("Мой размер ноги {0}", sizeLeg);
         }
     }
 }
