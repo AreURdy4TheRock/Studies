2c295830eb08cc8a9237a4d4c4f856cd68e1de55
AreURdy4TheRock <dorkwingdock@gmail.com>
2026-04-03 23:23:43 +0300

    Добавьте файлы проекта.

---
+++ Studies/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Studies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Art";
+            Console.WriteLine(MyName);
+        }
+    }
+}
