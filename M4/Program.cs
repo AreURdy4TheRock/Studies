22567d70a396f794b06a0579edae1f401d22f5b1
AreURdy4TheRock <dorkwingdock@gmail.com>
2026-04-06 22:46:39 +0300

    Начало 4 модуля

---
+++ M4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace M4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "A";
+            string B = "B";
+            bool C = A != B;
+            Console.WriteLine(C);
+        }
+    }
+}
