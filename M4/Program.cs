241d6df3eb2ef656e938859181b7936837aabc63
AreURdy4TheRock <dorkwingdock@gmail.com>
2026-04-06 22:54:16 +0300

    Добавлен метод который проверяет логическое выражение (A < B) | (X > Y)

--- M4/Program.cs
+++ M4/Program.cs
@@ -8,6 +8,16 @@
             string B = "B";
             bool C = A != B;
             Console.WriteLine(C);
+            AbXy();
+        }
+        static void  AbXy()
+        {
+            int A = 1;
+            int B = 2;
+            double X = 1.2;
+            double Y = 2.0;
+            bool c = (A < B) | (X > Y);
+            Console.WriteLine(c);
         }
     }
 }
