2f25a5b40827349374ed067f4b86be0e6bf02d7a
AreURdy4TheRock <dorkwingdock@gmail.com>
2026-05-09 18:50:23 +0300

    Итоговый првктикум в Main методе

--- M4/Program.cs
+++ M4/Program.cs
@@ -24,8 +24,43 @@ namespace M4
             //TwoArray();
             //TwoArraySum();
             //Cortej();
-            CortejPet();
+            //CortejPet();
 
+            (string Name, string LastName, string Login, int LoginLenght, bool HasPet, string[] FavColors, double Age) User;
+            for (int k = 0; k < 3; k++)
+            {
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+
+                User.LoginLenght = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var result = Console.ReadLine();
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.FavColors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.FavColors.Length; i++)
+                {
+                    User.FavColors[i] = Console.ReadLine();
+                }
+            }
 
         }
         static void  AbXy()
@@ -249,6 +284,8 @@ namespace M4
             Console.WriteLine("Вашего питомца зовут {0}. Это {1}. Ему {2} лет. Длина имени - {3}.", Pet.name, Pet.type, Pet.age, Pet.nameCount);
             Console.ReadKey();
         }
+
+
 
     }
 }
